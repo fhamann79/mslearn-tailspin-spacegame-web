ae3d6810e93175db51bf4af55b8e59b37cde497b
Fernando Hamann <fhamann@pucesa.edu.ec>
2020-09-30 14:03:32 -0500

    MEnsaje errores

--- Tailspin.SpaceGame.Web/Models/Model.cs
+++ Tailspin.SpaceGame.Web/Models/Model.cs
@@ -8,7 +8,7 @@ namespace TailSpin.SpaceGame.Web.Models
     public abstract class Model
     {
         // The value that uniquely identifies this object.
-        //Esta es una prueba de diseño 23
+        //Rama errores
         [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
     }
