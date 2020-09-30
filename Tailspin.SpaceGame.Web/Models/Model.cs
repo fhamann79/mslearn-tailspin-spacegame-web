fda8baa605d628a547bc358e0e371661b013ddaf
Fernando Hamann <fhamann@pucesa.edu.ec>
2020-09-30 13:50:17 -0500

    3

--- Tailspin.SpaceGame.Web/Models/Model.cs
+++ Tailspin.SpaceGame.Web/Models/Model.cs
@@ -8,7 +8,7 @@ namespace TailSpin.SpaceGame.Web.Models
     public abstract class Model
     {
         // The value that uniquely identifies this object.
-        //Esta es una prueba de diseño 2
+        //Esta es una prueba de diseño 23
         [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
     }
